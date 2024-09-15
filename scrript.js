2bab71a6b984ed7c5a49c4cbb86dc982f529cbed
Giridhar11411 <giridharhm11@gmail.com>
2024-09-15 22:21:44 +0530

    colortask

---
+++ scrript.js
@@ -0,0 +1,73 @@
+function myred(){
+    document.getElementById('text').style.backgroundColor='red'
+}
+function myaquamarine(){
+    document.getElementById('text').style.backgroundColor='aquamarine'
+}
+function myblue(){
+    document.getElementById('text').style.backgroundColor='blue'
+}
+function mybrown(){
+    document.getElementById('text').style.backgroundColor='brown'
+}
+function myburlywood(){
+    document.getElementById('text').style.backgroundColor='burlywood'
+}
+function mychartreuse(){
+    document.getElementById('text').style.backgroundColor='chartreuse'
+}
+function mydarkgoldenrod(){
+    document.getElementById('text').style.backgroundColor='darkgoldenrod'
+}
+function myfuchsia(){
+    document.getElementById('text').style.backgroundColor='fuchsia'
+}
+function myhotpink(){
+    document.getElementById('text').style.backgroundColor='hotpink'
+}
+function mycrimson(){
+    document.getElementById('text').style.backgroundColor='crimson'
+}
+function mycyan(){
+    document.getElementById('text').style.backgroundColor='cyan'
+}
+function mysilver(){
+    document.getElementById('text').style.backgroundColor='silver'
+}
+
+function mytextred(){
+    document.getElementById('text').style.color='red'
+}
+function mytextaquamarine(){
+    document.getElementById('text').style.color='aquamarine'
+}
+function mytextblue(){
+    document.getElementById('text').style.color='blue'
+}
+function mytextbrown(){
+    document.getElementById('text').style.color='brown'
+}
+function mytextburlywood(){
+    document.getElementById('text').style.color='burlywood'
+}
+function mytextchartreuse(){
+    document.getElementById('text').style.color='chartreuse'
+}
+function mytextdarkgoldenrod(){
+    document.getElementById('text').style.color='darkgoldenrod'
+}
+function mytextfuchsia(){
+    document.getElementById('text').style.color='fuchsia'
+}
+function mytexthotpink(){
+    document.getElementById('text').style.color='hotpink'
+}
+function mytextcrimson(){
+    document.getElementById('text').style.color='crimson'
+}
+function mytextcyan(){
+    document.getElementById('text').style.color='cyan'
+}
+function mytextsilver(){
+    document.getElementById('text').style.color='silver'
+}
